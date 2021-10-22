669b6651e37b07145e879d60adb80090c848f91c
Mehrdad Amini <pcmehrdad@gmail.com>
2021-10-23 03:09:53 +0330

    Update 02-common-requirements.sh

--- 02-common-requirements.sh
+++ 02-common-requirements.sh
@@ -2,6 +2,7 @@
 set -e
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 sudo apt --yes install software-properties-common aria2 bzip2 ca-certificates curl git gnupg gosu htop iotop iperf libcap2-bin libpng-dev make gcc nano net-tools nmap chrony openssh-server openssl p7zip poppler-utils apt-transport-https lsb-release python2 sqlite3 supervisor traceroute unar unzip wget zip zsh
+
 # sudo apt --yes install network-manager
 # sudo apt --yes dist-upgrade && sudo apt --yes update && sudo apt -q --yes upgrade
 sudo apt --yes update && sudo apt -q --yes upgrade
